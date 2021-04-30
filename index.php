5bbcf958a41f1198de1b72da2d3129cab2b4ed41
Andrea Marangon <marangonandrea84@gmail.com>
2021-04-30 15:16:28 +0200

    inizio esercizio

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+  
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>PHP Google FAQ</title>
+</head>
+<body>
+  <header>
+
+  </header>
+  <main>
+
+  </main>
+  <footer>
+
+  </footer>
+</body>
+</html>
