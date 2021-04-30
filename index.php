b753d34884d4a43638b7ac0a4112141ca95f35a8
Andrea Marangon <marangonandrea84@gmail.com>
2021-04-30 18:08:52 +0200

    lavoro su main

--- index.php
+++ index.php
@@ -72,32 +72,37 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>PHP Google FAQ</title>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="./dist/css/master.css">
 </head>
 <body>
   <header>
 
   </header>
   <main>
-    <?php
-    foreach ($faqs as $question => $answers) {
-    ?>
+    <div class="container_main">
+      <?php
+      foreach ($faqs as $question => $answers) {
+      ?>
 
-      <div class="">
-        <h1><?php echo $question ?></h1>
-      </div>
+        <div class="">
+          <h2><?php echo $question ?></h2>
+        </div>
 
-     <?php
-      foreach ($answers as $answer) {
-     ?>
+       <?php
+        foreach ($answers as $answer) {
+       ?>
 
-       <div class="">
-         <p><?php echo $answer ?></p>
-       </div>
+         <div class="">
+           <p><?php echo $answer ?></p>
+         </div>
 
-     <?php
+       <?php
+        }
       }
-    }
-    ?>
+      ?>
+    </div>
 
 
 
